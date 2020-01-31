2de43c2d7f2b8f9eda76bc74f80db5d1f84c6aa8
Adam Alicki <adamalicki@Adams-MacBook-Pro.local>
2020-01-31 16:33:24 -0500

    Adding in Laravel correction

--- php-laravel.php
+++ php-laravel.php
@@ -1,34 +1,35 @@
+<?php
 /**
-* Store a newly Job Opening in jobboard.jobs
-*
-* @param  \Illuminate\Http\Request  $request
-*/
+ * Store a newly Job Opening in jobboard.jobs
+ *
+ * @param  \Illuminate\Http\Request  $request
+ */
 
-    public function store(Request $request)
-    {
+public function store(Request $request)
+{
 
-        //Load the employer profile for the logged in user.
-        $employer = Employers::where('user_id', Auth::id())->first();
+    //Load the employer profile for the logged in user.
+    $employer = Employers::where('user_id', Auth::id())->first();
 
-        //Check if we have maxed out the number of openings we can have
-        if ($this->activeJobCount($employer->id) >= $employer->max_listings) {
-        
-            return response()->json(['limit_reached' => true], 422);
-        } else {
+    //Check if we have maxed out the number of openings we can have
+    if ($this->activeJobCount($employer->id) >= $employer->max_listings) {
 
-            $job = new Jobs();
-            $job->employers_id      = $employer->id;
-            $job->title             = $request->get('title');
-            $job->description       = $request->get('description');
-            $job->pay               = $request->get('pay'); //$65 hr, 50,000 year (any format)
-            $job->type              = $request->get('type'); //front-end, back-end, etc
-            $job->location          = $request->get('location');
-            $job->category_id       = $request->get('category');
-            $job->apply_link        = $request->get('apply_link'); //link on employer's site to apply
-            $job->active            = ;
-            $job->save();
+        return response()->json(['limit_reached' => true], 422);
+    } else {
 
-            //Return JSON response of a success
-            return response()->json(null, 200);
-        }
+        $job = new Jobs();
+        $job->employers_id      = $employer->id;
+        $job->title             = $request->get('title');
+        $job->description       = $request->get('description');
+        $job->pay               = $request->get('pay'); //$65 hr, 50,000 year (any format)
+        $job->type              = $request->get('type'); //front-end, back-end, etc
+        $job->location          = $request->get('location');
+        $job->category_id       = $request->get('category');
+        $job->apply_link        = $request->get('apply_link'); //link on employer's site to apply
+        $job->active            = ;
+        $job->save();
+
+        //Return JSON response of a success
+        return response()->json(null, 200);
     }
+}
