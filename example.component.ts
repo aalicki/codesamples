7d5f6248d38bd7014ebe6a71347aa99c5bf17205
Adam Alicki <adamalicki@Adams-MacBook-Pro.local>
2020-01-31 19:25:54 -0500

    Improved Angular component example.

--- example.component.ts
+++ example.component.ts
@@ -1,37 +1,79 @@
-/**
- * Total Owed
- *
- * Loop through an array of items and get their cost,
- * then add them all up to output into the front-end
- */
-totalOwed () {
+import {
+    Component,
+    OnInit,
+    Input,
+    ViewChild
+} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient}     from '@angular/common/http';
 
-    let due;
+@Component({
+    selector: 'main',
+    templateUrl: './example.component.html',
+    styleUrls: ['./example.component.css']
+})
 
-    this.itemList.forEach(item => {
-        due += item.cost;
-    });
+export class ExampleComponent implements OnInit {
+    @Input() outsideVar = 'Testing';
+    itemList: any[];
+    items: any[];
+    removedItems: any[];
+    amountOwed = 0;
+    showExampleButton = true;
 
-    this.amountOwed = parseInt(due, 10);
-}
+    /**
+     *
+     * @param route
+     * @param http
+     */
+    constructor(
+        public route: ActivatedRoute,
+        public http: HttpClient) {
+    }
 
-/**
- * Remove items without a name, then
- * adds them into a removed item list
- * incase we need to remember which were removed
- */
-removeItems () {
+    onInt() {
 
-    // this.items are an array of various products
+        this.showExampleButton = false; // Let's hide the button!
 
-    // Reiterate to remove categories with no specs.
-    this.items.forEach((item, index) => {
+        console.log('Testing this example component!');
+    }
 
-        if (!item.name.length) {
-            this.item.splice(index, 1);
+    /**
+     * Total Owed
+     *
+     * Loop through an array of items and get their cost,
+     * then add them all up to output into the front-end
+     */
+    totalOwed() {
 
-            // now push it to a remove item list for tracking
-            this.removedItems.push(item);
-        }
-    });
+        let due;
+
+        this.itemList.forEach(item => {
+            due += item.cost;
+        });
+
+        this.amountOwed = parseInt(due, 10);
+    }
+
+    /**
+     * Remove items without a name, then
+     * adds them into a removed item list
+     * incase we need to remember which were removed
+     */
+    removeItems() {
+
+        // this.items are an array of various products
+
+        // Reiterate to remove categories with no specs.
+        this.items.forEach((item, index) => {
+
+            if (!item.name.length) {
+                this.items.splice(index, 1);
+
+                // now push it to a remove item list for tracking
+                this.removedItems.push(item);
+            }
+        });
+    }
+
 }
